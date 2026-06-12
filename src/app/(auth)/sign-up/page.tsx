f7663d62c9cb64918ebc7030af0ee9caee1945c6
Charu <charu@Charus-MacBook-Air.local>
2026-06-12 15:47:41 +0530

    Added Signup Page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,220 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, Form, useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceCallback, useDebounceValue } from "usehooks-ts";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schema/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const signUppage = () => {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debounced = useDebounceCallback(setUsername, 300);
+
+  const router = useRouter();
+  //zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    // fn
+    const checkUsernameUnique = async () => {
+      if (username) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${username}`,
+          );
+
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username",
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    // call the upper fn
+    checkUsernameUnique();
+  }, [username]);
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      const response = await axios.post<ApiResponse>("/api/sign-up", data);
+
+      toast("Success", {
+        description: response.data.message,
+      });
+
+      router.replace(`/verify/${username}`);
+    } catch (error) {
+      console.log("Error in signup page:", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+
+      let errorMessage = axiosError.response?.data.message;
+
+      toast("Signup failed", {
+        description: errorMessage,
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-400 via-purple-300 to-pink-300">
+      <div className="w-full max-w-md p-8 space-y-8 bg-amber-50 rounded-lg shadow-md">
+        {/* top ui part */}
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-purple-900">
+            Join Anonymous Feedback
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
+        {/* form ui part */}
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <Controller
+            name="username"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Username</FieldLabel>
+
+                <Input
+                  {...field}
+                  id={field.name}
+                  aria-invalid={fieldState.invalid}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    debounced(e.target.value);
+                  }}
+                />
+
+                {isCheckingUsername && <Loader2 className="animate-spin" />}
+
+                {!isCheckingUsername && usernameMessage && (
+                  <p
+                    className={`text-sm ${
+                      usernameMessage === "Username is unique" ?
+                        "text-green-500"
+                      : "text-red-500"
+                    }`}
+                  >
+                    {usernameMessage}
+                  </p>
+                )}
+
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+
+                <Input
+                  {...field}
+                  id={field.name}
+                  aria-invalid={fieldState.invalid}
+                  type="email"
+                />
+
+                <FieldDescription>
+                  We will send you a verification code
+                </FieldDescription>
+
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+
+                <Input
+                  {...field}
+                  id={field.name}
+                  aria-invalid={fieldState.invalid}
+                  type="password"
+                />
+
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="w-full bg-purple-950"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ?
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </>
+            : "Sign Up"}
+          </Button>
+        </form>
+
+        {/* bottom ui */}
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{" "}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default signUppage;
